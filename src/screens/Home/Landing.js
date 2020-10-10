548be7e90eb244cddf8ff6f9602a9008254c35e3
Opeyemi Agbeja <niyola59@gmail.com>
2020-10-10 14:22:42 +0100

    update user gps location

--- src/screens/Home/Landing.js
+++ src/screens/Home/Landing.js
@@ -1,7 +1,18 @@
 /* eslint-disable no-shadow */
 /* eslint-disable react-native/no-inline-styles */
 import React, {useEffect} from 'react';
-import {View, StatusBar, TouchableOpacity, Image, Text} from 'react-native';
+import {
+  View,
+  StatusBar,
+  TouchableOpacity,
+  Image,
+  Platform,
+  Alert,
+  PermissionsAndroid,
+  ToastAndroid,
+  Linking,
+  Text,
+} from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 import styles from './styles/dashboard_styles';
 import {Basestyle, Images} from '../../helpers/BaseThemes';
@@ -14,6 +25,8 @@ import {
   updateUserInfo,
   getCategories,
 } from '../../actions/auth.action';
+import Geolocation from 'react-native-geolocation-service';
+
 const Landing = ({
   navigation,
   saveUserInfo,
@@ -32,8 +45,116 @@ const Landing = ({
     !user_info && checkUser();
     !categories && getCategories();
     device_id && updateUserInfo({oneSignalPlayerId: device_id});
+    getLocation();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const getLocation = async () => {
+    const hasLocationPermission = await checkLocationPermission();
+
+    if (!hasLocationPermission) {
+      return;
+    }
+
+    Geolocation.getCurrentPosition(
+      (position) => {
+        //console.log(position);
+        const data = {
+          gpsLocation: [
+            {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            },
+          ],
+        };
+        updateLocation(data);
+      },
+      (error) => {
+        console.log(error);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000,
+      },
+    );
+  };
+  const updateLocation = async (data) => {
+    try {
+      await updateUserInfo(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  const checkLocationPermission = async () => {
+    if (Platform.OS === 'ios') {
+      const hasPermission = await hasLocationPermissionIOS();
+      return hasPermission;
+    }
+
+    if (Platform.OS === 'android' && Platform.Version < 23) {
+      return true;
+    }
+
+    const hasPermission = await PermissionsAndroid.check(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+
+    if (hasPermission) {
+      return true;
+    }
+
+    const status = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+
+    if (status === PermissionsAndroid.RESULTS.GRANTED) {
+      return true;
+    }
+
+    if (status === PermissionsAndroid.RESULTS.DENIED) {
+      ToastAndroid.show(
+        'Location permission denied by user.',
+        ToastAndroid.LONG,
+      );
+    } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+      ToastAndroid.show(
+        'Location permission revoked by user.',
+        ToastAndroid.LONG,
+      );
+    }
+
+    return false;
+  };
+  const hasLocationPermissionIOS = async () => {
+    const openSetting = () => {
+      Linking.openSettings().catch(() => {
+        Alert.alert('Unable to open settings');
+      });
+    };
+    const status = await Geolocation.requestAuthorization('whenInUse');
+
+    if (status === 'granted') {
+      return true;
+    }
+
+    if (status === 'denied') {
+      Alert.alert('Location permission denied');
+    }
+
+    if (status === 'disabled') {
+      Alert.alert(
+        'Turn on Location Services to allow RendaMerchant to determine your location.',
+        '',
+        [
+          {text: 'Go to Settings', onPress: openSetting},
+          {text: "Don't Use Location", onPress: () => {}},
+        ],
+      );
+    }
+
+    return false;
+  };
   return (
     <View style={Basestyle.container}>
       <StatusBar
